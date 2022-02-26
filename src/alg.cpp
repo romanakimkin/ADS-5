9731773a83a3c19be60bccd2b8d5b2ad9404e07d
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2022-02-26 21:59:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,10 @@
 
 bool checkPrime(uint64_t value) {
     if(value <= 1){
-        return false
+        return false;
     }
     if(value == 2){
-        return true
+        return true;
     }
     int a = 0;
     for(int i = 2; i < value; i++){
@@ -25,7 +25,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    int b = 0; c = 0;
+    int b = 0, c = 0;
     while(true){
             b++;
         if(checkPrime(b))
@@ -47,7 +47,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int sum = 0;
+    uint64_t sum = 0;
     for(int i = 0; i < hbound; i++){
     if(checkPrime(i))
         sum += i;
