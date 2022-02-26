5dada84d8723b11cf164ce7845df018e50847337
romanakimkin <78901819+romanakimkin@users.noreply.github.com>
2022-02-26 21:46:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,53 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if(value <= 1){
+        return false
+    }
+    if(value == 2){
+        return true
+    }
+    int a = 0;
+    for(int i = 2; i < value; i++){
+        if ((value % i) == 0){
+            a = 1;
+            break;
+        }
+    }
+    if(a == 0){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int b = 0; c = 0;
+    while(true){
+            b++;
+        if(checkPrime(b))
+            c++;
+        if(c == n)
+            return b;
+    }
+    return c;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    while(true){
+        value++;
+        if(checkPrime(value)){
+            return value;
+        }
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int sum = 0;
+    for(int i = 0; i < hbound; i++){
+    if(checkPrime(i))
+        sum += i;
+    return sum;
+    }
 }
